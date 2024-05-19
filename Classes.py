40de7608b965ac1dd31856923e98f47a3fd10556
Fredson Arthur <toortuugaa805@gmail.com>
2024-05-19 09:35:29 -0300

    Troca de falando para andando

--- Classes.py
+++ Classes.py
@@ -6,68 +6,68 @@ class Pessoa():
         self.peso = peso
         self.idade = idade
         self.comendo = False
-        self.falando = False
+        self.andando = False
         self.dormindo = False
 
 #Criação dos metodos de ações e suas verificações
     def comer(self):
 
-        if self.falando == True:
+        if self.andando == True:
 
             self.comendo = False
             self.dormindo = False
-            print(f"O {self.nome} não comer agora pois está falando.")
+            print(f"O {self.nome} não comer agora pois está andando.")
         
         elif self.dormindo == True:
 
             self.comendo = False
-            self.falando = False
+            self.andando = False
             print(f"O {self.nome} não pode comer agora pois está dormindo.")
         
         else:
             self.comendo = True
-            self.falando = False
+            self.andando = False
             self.dormindo = False
             print(f"O {self.nome} está comendo no momento.")
 
-    def falar(self):
+    def andar(self):
 
         if self.comendo == True:
 
-            self.falando = False
+            self.andando = False
             self.dormindo = False
-            print(f"O {self.nome} não pode falar agora pois está comendo.")
+            print(f"O {self.nome} não pode andar agora pois está comendo.")
         
         elif self.dormindo == True:
 
             self.comendo = False
-            self.falando = False
-            print(f"O {self.nome} não pode falar agora pois está dormindo.")
+            self.andando = False
+            print(f"O {self.nome} não pode andar agora pois está dormindo.")
         
         else:
 
-            self.falando = True
+            self.andando = True
             self.comendo = False
             self.dormindo = False
-            print(f"O {self.nome} está falando no momento.")
+            print(f"O {self.nome} está andando no momento.")
 
     def dormir(self):
 
         if self.comendo == True:
 
-            self.falando = False
+            self.andando = False
             self.dormindo = False
             print(f"O {self.nome} não pode dormir agora pois está comendo.")
         
-        elif self.falando == True:
+        elif self.andando == True:
 
             self.comendo = False
             self.dormindo = False
-            print(f"O {self.nome} não pode falar agora pois está dormindo.")
+            print(f"O {self.nome} não pode andar agora pois está dormindo.")
 
         else:
             
             self.dormindo = True
             self.comendo = False
-            self.falando = False
+            self.andando = False
             print(f"O {self.nome} está dormindo no momento")
