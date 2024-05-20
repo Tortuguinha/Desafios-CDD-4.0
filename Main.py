5481d5ede74d3a076318bd8ba433c9f60ac4ccfb
Fredson Arthur <toortuugaa805@gmail.com>
2024-05-20 15:21:29 -0300

    Adição de loop ate encerramento do codigo

--- Main.py
+++ Main.py
@@ -16,10 +16,16 @@ if escolha_inicio == 1:
     escolha_parar = int(input("Deseja parar a ação? \n 1 para sim \n 2 para não."))
     
     if escolha_parar == 1:
-        pou.parar()
+        pou.parar_comer()
     
     elif escolha_parar == 2:
-        print(f"{pou.nome} ainda está comendo.")
+        print(f"{pou.nome} ainda está comendo. Saco vazio não para em pé!")
+
+        while escolha_parar == 2:
+            escolha_parar = int(input("Deseja parar a ação? \n 1 para sim \n 2 para não."))
+            
+            if escolha_parar == 1:
+                pou.parar_comer()
     
     elif escolha_parar != 0:
         print("Escolha inexistente.")
@@ -30,11 +36,17 @@ elif escolha_inicio == 2:
     escolha_parar = int(input("Deseja parar a ação? \n 1 para sim \n 2 para não."))
     
     if escolha_parar == 1:
-        pou.parar()
+        pou.parar_andar()
     
     elif escolha_parar == 2:
-        print(f"{pou.nome} ainda está comendo.")
-    
+        print(f"{pou.nome} ainda está andando. Bora chegar na africa a pé!")
+
+        while escolha_parar == 2:
+            escolha_parar = int(input("Deseja parar a ação? \n 1 para sim \n 2 para não."))
+
+            if escolha_parar == 1:
+                pou.parar_andar()
+
     elif escolha_parar != 0:
         print("Escolha inexistente.")
 
@@ -44,11 +56,17 @@ elif escolha_inicio == 3:
     escolha_parar = int(input("Deseja parar a ação? \n 1 para sim \n 2 para não."))
     
     if escolha_parar == 1:
-        pou.parar()
+        pou.parar_dormir()
     
     elif escolha_parar == 2:
-        print(f"{pou.nome} ainda está comendo.")
+        print(f"{pou.nome} ainda está comendo. Belo adormecido!")
+
+        while escolha_parar == 2:
+            escolha_parar = int(input("Deseja parar a ação? \n 1 para sim \n 2 para não."))
     
+            if escolha_parar == 1:
+                pou.parar_dormir()
+
     elif escolha_parar != 0:
         print("Escolha inexistente.")
 
