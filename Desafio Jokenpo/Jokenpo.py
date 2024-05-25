c599e5dd0e61e02b8195b8864b3c61b44faf7330
Fredson Arthur <167770788+Tortuguinha@users.noreply.github.com>
2024-05-25 15:16:07 -0300

    Update Jokenpo.py

--- Desafio Jokenpo/Jokenpo.py
+++ Desafio Jokenpo/Jokenpo.py
@@ -8,18 +8,31 @@ class JogoPedraPapelTesoura:
         self.resultado = {'pedra': {'tesoura': 'Que pena, pedra quebra tesoura!', 'papel': 'Parabéns, papel cobre pedra!'},
                           'papel': {'pedra': 'Que pena, papel cobre pedra!', 'tesoura': 'Parabéns, tesoura corta papel!'},
                           'tesoura': {'pedra': 'Parabéns, pedra quebra tesoura!', 'papel': 'Que pena, tesoura corta papel!'}}
+        self.pontos_jogador1 = 0
+        self.pontos_jogador2 = 0
 
     def jogar(self):
-        escolha_jogador1 = self.jogador1.fazer_escolha(self.opcoes)
-        escolha_jogador2 = self.jogador2.fazer_escolha(self.opcoes)
+        rodadas = 0
+        while rodadas < 3 and not (self.pontos_jogador1 == 2 and self.pontos_jogador2 == 0) and not (self.pontos_jogador2 == 2 and self.pontos_jogador1 == 0):
+            rodadas += 1
+            escolha_jogador1 = self.jogador1.fazer_escolha(self.opcoes)
+            escolha_jogador2 = self.jogador2.fazer_escolha(self.opcoes)
 
-        print(f"{self.jogador1.nome} escolheu: {escolha_jogador1}")
-        print(f"{self.jogador2.nome} escolheu: {escolha_jogador2}")
+            print(f"{self.jogador1.nome} escolheu: {escolha_jogador1}")
+            print(f"{self.jogador2.nome} escolheu: {escolha_jogador2}")
 
-        if escolha_jogador1 == escolha_jogador2:
-            print("Empate!")
-        elif self.resultado[escolha_jogador1][escolha_jogador2]:
-            print(self.resultado[escolha_jogador1][escolha_jogador2])
+            if escolha_jogador1 == escolha_jogador2:
+                print("Empate!")
+            elif self.resultado[escolha_jogador1][escolha_jogador2]:
+                print(self.resultado[escolha_jogador1][escolha_jogador2])
+                self.pontos_jogador1 += 1
+            else:
+                print(self.resultado[escolha_jogador2][escolha_jogador1])
+                self.pontos_jogador2 += 1
+
+        if self.pontos_jogador1 > self.pontos_jogador2:
+            print(f"{self.jogador1.nome} venceu com {self.pontos_jogador1} pontos!")
+        elif self.pontos_jogador2 > self.pontos_jogador1:
+            print(f"{self.jogador2.nome} venceu com {self.pontos_jogador2} pontos!")
         else:
-            print(self.resultado[escolha_jogador2][escolha_jogador1])
-
+            print("O jogo terminou em empate!")
